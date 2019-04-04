76ea9eff95a1723d722151a89712193d03f0b909
xaviguasch <xaviguasch@gmail.com>
2019-04-04 15:47:20 +0200

    sent data back and forth between server and client

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,5 +3,9 @@ const socket = io()
 
 socket.on('countUpdated', (count) => {
     console.log('The count has been updated!', count);
+})
 
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked');
+    socket.emit('increment')
 })
--- src/index.js
+++ src/index.js
@@ -25,6 +25,12 @@ io.on('connection', (socket) => {
 
 
     socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
 })
 
 
