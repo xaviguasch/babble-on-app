d066cb83b51a6d8b7b16af95f3a1f499d0368682
xaviguasch <xaviguasch@gmail.com>
2019-04-08 15:15:05 +0200

    implemented profanity filter with bad-words npm package

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,8 +21,13 @@ socket.on('messageLoc', (message) => {
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message, () => {
-        console.log('the message was delivered');
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log('Message delivered!!!!');
+
     })
 })
 
